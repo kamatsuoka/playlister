42fd95e6bec7774039280f518ebce754552afa82
Kenji Matsuoka <kenjima@alexa.com>
2021-09-04 15:02:42 -0700

    using password hash stored separately in script properties, instead of stored in source code

--- src/server/auth-server.js
+++ src/server/auth-server.js
@@ -1,16 +1,20 @@
 const VALIDATED = 'validated'
-const PASSWORD_HASH = 'mtBO93jfSmM/wBFk1rSFzgxsv3yr0EBeyesLN8L2XylbWkOEpZ8ebovEjGXD4uRXwSATzZHVXWIBa27r809chw=='
 
 /**
- * Checks if the password is valid
+ * Checks if the password is valid.
+ *
+ * Relies on PASSWORD_HASH being set in script properties
  *
  * @throws {Error} if password invalid
+ * @see https://developers.google.com/apps-script/guides/properties
  */
 export const checkPassword = password => {
   const digest = Utilities.computeDigest(Utilities.DigestAlgorithm.SHA_512, password, Utilities.Charset.US_ASCII)
   const hash = Utilities.base64Encode(digest)
-  console.log(`hash = ${hash}`)
-  if (hash === PASSWORD_HASH) {
+  console.log(`input hash = ${hash}`)
+  const passwordHash = PropertiesService.getScriptProperties().getProperty('PASSWORD_HASH')
+  console.log(`password hash = ${passwordHash}`)
+  if (hash === passwordHash) {
     PropertiesService.getUserProperties().setProperty(VALIDATED, 'true')
   } else {
     PropertiesService.getUserProperties().setProperty(VALIDATED, 'false')
